3100bf10247bbf9fb5f173c7322a272163014e27
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2021-11-28 21:19:53 -0300

    h

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 const Api = {
-  apiUrl: 'https://produtback.herokuapp.com',
+  apiUrl: 'https://app-pok.herokuapp.com/',
   fetchGetAll: () => fetch(Api.apiUrl),
   fetchGetById: (id) => fetch(`${Api.apiUrl}/${id}`),
   fetchPost: (data) => {
