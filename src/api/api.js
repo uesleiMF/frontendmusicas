13bcd4a9850224cfd226d92fec19eec09001b023
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2021-11-29 14:14:32 -0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 const Api = {
-  apiUrl: 'http://localhost:3001/musicas',
+  apiUrl: 'https://app-pok.herokuapp.com/musicas',
   fetchGetAll: () => fetch(Api.apiUrl),
   fetchGetById: (id) => fetch(`${Api.apiUrl}/${id}`),
   fetchPost: (data) => {
