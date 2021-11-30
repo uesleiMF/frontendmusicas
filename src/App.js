bc2b6f7bfc2a79d8ddae9a5fafee9886069dc835
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2021-11-30 10:13:19 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,23 +6,34 @@ import { Routes, Route } from 'react-router-dom';
 import View from './pages/View/View';
 import Edit from './pages/Edit/Edit';
 
-// Route - Define qual componente(element) vai aparecer na tela de acordo com o seu caminho (path);
-// Routes - renderiza o elemento que melhor correponde a rota atual.
 
-// componentes - é um bloco de codigo  e logica que pode ser reutilizado e diversos locais da aplicação
-// pages - é um componente geral que pode conter outros componentes que contem uma estrutura de pagina.
+
+
+
+
+
 function App() {
   return (
-    <div>
+    
+            <div>   
+        
       <Header/>
+      
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home/>}   />
         <Route path="/cadastro" element={<Cadastro/>}/>
         <Route path="/view/:id" element={<View/>}/>
         <Route path="/edit/:id" element={<Edit/>}/>
+       
       </Routes>
-    </div>
+
+      
+      </div>
+
+        
+         
   );
 }
+
 
 export default App;
