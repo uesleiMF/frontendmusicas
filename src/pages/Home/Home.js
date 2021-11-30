2a7a89f187ccb4275faa7684d3811f5cf817b4cd
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2021-11-30 10:14:05 -0300

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,11 +3,30 @@ import ListProdut from '../../components/structure/ListProdut';
 
 const Home = () => {
   return (
+   
+     
     <div className="container">
-      <h1 className="text-center h1">BLUE MUSICAS</h1>
-      <ListProdut/>
+     <div className="row mb-3">
+    <div class="container bg-info">
+  <div class="nome text-center">
+  <div className="col-9">
+  
+</div>
+<div class="p-1 mb-2 bg-info text-black">
+    
+    <h1 className="text-center h1">CONHEÇA NOSSOS PRODUTOS</h1>
+    <ListProdut/>
+     
+
+      </div>
+     </div>
     </div>
+   </div>
+  </div>
   )
 }
+
+
+
 
 export default Home
