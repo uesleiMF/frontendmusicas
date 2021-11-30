ac8df563e99cf3fff4a1386f3d47233bfe1d87f1
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2021-11-30 15:00:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Cadastro from './pages/Cadastro/Cadastro';
 import { Routes, Route } from 'react-router-dom';
 import View from './pages/View/View';
 import Edit from './pages/Edit/Edit';
-
+import Inicio from './pages/Inicio/Inicio';
 
 
 function App() {
@@ -20,6 +20,7 @@ function App() {
         <Route path="/cadastro" element={<Cadastro/>}/>
         <Route path="/view/:id" element={<View/>}/>
         <Route path="/edit/:id" element={<Edit/>}/>
+        <Route path="/Inicio" element={<Inicio/>}/>
        
       </Routes>
 
