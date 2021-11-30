921198533bf22bd77b2f0128a595cf6b5820fd63
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2021-11-30 09:24:01 -0300

    Update index.js

--- src/components/structure/ListMusic/index.js
+++ src/components/structure/ListMusic/index.js
@@ -2,27 +2,27 @@ import React, { useState, useEffect } from 'react'
 import Card from '../Card';
 import Api from '../../../api/api';
 // Ele vai fazer uma requisicao para a API(/musicas) e vai listar em varios card.
-const ListMusic = () => {
+const ListProdut = () => {
   // estado - memoria do componente
-  const [musicas, setMusicas] = useState([]);
+  const [produtos, setProdutos] = useState([]);
   // const [count, setCount] = useState(0);
 
   // UseEffect
   // criar o componente ou quando o componete ou o estado tem uma atualizacao.
   useEffect(() => {
-    getMusicas();
+    getProdutos();
   }, [])
 
   // useEffect(() => {
   //   document.title = `voce clicou ${count}`;
   // }, [count])
 
-  const getMusicas = async () => {
+  const getProdutos = async () => {
     const request = await Api.fetchGetAll();
     // data = recebe os dados da api (musicas).
     const data = await request.json();
     // atualizo meu estado em memoria com as musicas - para atualizar no DOM.
-    setMusicas(data);
+    setProdutos(data);
   }
 
   // const handleButton = () => {
@@ -34,11 +34,11 @@ const ListMusic = () => {
     <div className="row row-cols-1 row-cols-md-3 mt-3 g-4">
       {/* <button onClick={handleButton}>Incrementa</button>
       <p>{count}</p> */}
-      {musicas.map((musica) => (
-        <Card data={musica} key={musica._id}/>
+      {produtos.map((produto) => (
+        <Card data={produto} key={produto._id}/>
       ))}
     </div>
   )
 }
 
-export default ListMusic
+export default ListProdut
