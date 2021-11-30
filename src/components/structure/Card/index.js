de8bd98d626acd2284d3b29ddfa4aced09206977
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2021-11-30 09:23:55 -0300

    Update index.js

--- src/components/structure/Card/index.js
+++ src/components/structure/Card/index.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Card = (props) => {
   // objeto musica
-  const musica = props.data;
+  const produto = props.data;
   return (
-    <Link to={`/view/${musica._id}`} className="col">
+    <Link to={`/view/${produto._id}`} className="col">
       <div className="card">
-        <img src={musica.capa} alt={musica.nome} className="card-img-top" />
+        <img src={produto.capa} alt={produto.nome} className="card-img-top" />
         <div className="card-body">
-          <h5 className="card-title">{musica.nome}</h5>
-          <span className="badge bg-primary">{musica.genero}</span>
+          <h5 className="card-title">{produto.nome}</h5>
+          <span className="badge bg-primary">{produto.genero}</span>
         </div>
       </div>
     </Link>
